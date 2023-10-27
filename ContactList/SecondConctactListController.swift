159d67898e4ed67e19b1e6ddde9c46d49d10785b
John Doe <sergey@itikho.com>
2023-10-27 17:53:15 +0300

    part two without shuffle

--- ContactList/SecondConctactListController.swift
+++ ContactList/SecondConctactListController.swift
@@ -8,36 +8,37 @@
 import UIKit
 
 class SecondConctactListController: UITableViewController {
+    
     private var contactList = Person.getPersonsList()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
 }
 
 extension SecondConctactListController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         contactList.count
     }
-    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        contactList[section].name
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "anotherCells", for: indexPath)
-        
-        let contact = contactList[indexPath.row]
+        let contact = contactList[indexPath.section]
         var content = cell.defaultContentConfiguration()
-        
-        content.text = contact.name
+        if indexPath.row == 0 {
+            content.text = contact.email
+        } else {
+            content.text = contact.phone
+        }
         cell.contentConfiguration = content
-        
         return cell
     }
-    
-    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         40
     }
-    
-
 }
